3c9c9056bb55d4f0dfbc02aa5a98d98826cf1096
Stefan Weitlaner <steff.weiti@gmail.com>
2020-11-19 08:21:13 +0100

    Changed date format to GxP format Added hideNotification on BTN3 press

--- apps/cliock/app.js
+++ apps/cliock/app.js
@@ -2,50 +2,62 @@ var fontsize = 3;
 var locale = require("locale");
 var marginTop = 40;
 var flag = false;
-var WeekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var WeekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function drawAll(){
-  updateTime();
-  updateRest(new Date());
+function drawAll() {
+    updateTime();
+    updateRest(new Date());
 }
 
-function updateRest(now){
-  let date = locale.date(now,false);
-  writeLine(WeekDays[now.getDay()],1);
-  writeLine(date,2);
+function updateRest(now) {
+    let date = locale.date(now, false);
+    writeLine(WeekDays[now.getDay()], 1);
+    let darr = date.split(" ");
+    let tmp = darr[0];
+    darr[0] = darr[1];
+    darr[1] = tmp;
+    writeLine(darr.join("-"), 2);
 }
-function updateTime(){
-  if (!Bangle.isLCDOn()) return;
-  let now = new Date();
-  let h = now.getHours();
-  let m = now.getMinutes();
-  h = h>=10?h:"0"+h;
-  m = m>=10?m:"0"+m;
-  writeLine(h+":"+m,0);
-  writeLine(flag?" ":"_",3);
-  flag = !flag;
-  if(now.getMinutes() == 0)
-    updateRest(now);
+
+function updateTime() {
+    if (!Bangle.isLCDOn()) return;
+    let now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    h = h >= 10 ? h : "0" + h;
+    m = m >= 10 ? m : "0" + m;
+    writeLine(h + ":" + m, 0);
+    writeLine(flag ? " " : "_", 3);
+    flag = !flag;
+    if (now.getMinutes() == 0)
+        updateRest(now);
 }
-function writeLineStart(line){
-  g.drawString(">",4,marginTop+line*30);
+
+function writeLineStart(line) {
+    g.drawString(">", 4, marginTop + line * 30);
 }
-function writeLine(str,line){
-  g.setFont("6x8",fontsize);
-  g.setColor(0,1,0);
-  g.setFontAlign(-1,-1);
-  g.clearRect(0,marginTop+line*30,((str.length+1)*20),marginTop+25+line*30);
-  writeLineStart(line);
-  g.drawString(str,25,marginTop+line*30);
-} 
+
+function writeLine(str, line) {
+    g.setFont("6x8", fontsize);
+    g.setColor(0, 1, 0);
+    g.setFontAlign(-1, -1);
+    g.clearRect(0, marginTop + line * 30, ((str.length + 1) * 20), marginTop + 25 + line * 30);
+    writeLineStart(line);
+    g.drawString(str, 25, marginTop + line * 30);
+}
+
+function hideNotification() {
+    require("notify").hide();
+}
 
 g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
 drawAll();
-Bangle.on('lcdPower',function(on) {
-  if (on)
-    drawAll();
+Bangle.on('lcdPower', function(on) {
+    if (on)
+        drawAll();
 });
 var click = setInterval(updateTime, 1000);
-setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
+setWatch(Bangle.showLauncher, BTN2, { repeat: false, edge: "falling" });
+setWatch(hideNotification, BTN3, { repeat: true, edge: "falling" });
